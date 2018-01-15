abf4f2363f9a45a12f25a610d9019a1cb96e7764
zhdoudou <2795130683@qq.com>
2018-01-15 14:17:00 +0800

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	Application/Home/Controller/UserController.class.php

--- Application/Home/Model/SpreadModel.class.php
+++ Application/Home/Model/SpreadModel.class.php
@@ -6,7 +6,7 @@ class SpreadModel extends Model
 {
 
     Protected $autoCheckFields = false;
-<<<<<<< HEAD
+
 
 	/**
 	 * 查询出某个会员消费总金额及数量
@@ -14,19 +14,13 @@ class SpreadModel extends Model
 	 * @date 2018-1-13
 	 * @return void
 	 */
-	public function getPriceOrNum($user_id)
-    {
-    	$rst1 = M('order')->where(['user_id' => $user_id])->field('SUM(commodity_price) as sum')->find();
-
-		return ['sumprice' => $rst1['sum']];
-=======
+	
 	
 	public function getSumPrice($user_id)
     {
     	$rst=M('order')->where(['user_id' => $user_id])->field('SUM(commodity_price)')->find();
 		
 		return ['sumprice' => $rst['sum(commodity_price)']];
->>>>>>> origin/master
     }
 
     
