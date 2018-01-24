16dcfbecfc0bec9a7d209b7bc2b2366dff3427b3
zhdoudou <2795130683@qq.com>
2018-01-24 20:47:03 +0800

    修复2

---
+++ Application/Admin/Controller/SiteController.class.php
@@ -0,0 +1,41 @@
+<?php
+namespace Admin\Controller;
+use Think\Controller;
+
+/**
+ * @author zh <[<email address>]>
+ * @since 2018.1.7 
+ */
+class SiteController extends PrivilegeController
+{
+	/**
+	 * 域名设置页面
+	 * @author zh <[<email address>]>
+	 * @return [type] [description]
+	 */
+    public function index()
+    {
+    	
+        $this->display();
+    }
+    
+  
+
+    /**
+     * 处理域名编辑请求
+     * @author zh 
+     * @return [type] [description]
+     */
+    public function site()
+    {
+    	$site=I('post.site');
+		/*pr($site);die;*/
+    	if (!$site) {
+    		$this->error('请填写域名!');
+    	} 
+    	C('HOST_HOME',$site);
+		
+		$this->success('域名设置成功!',U('Site/site'),2);
+    }
+    
+}
