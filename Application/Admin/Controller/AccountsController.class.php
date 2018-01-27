5b63bfbc75631976904da0b1c4bba757b8925796
zhdoudou <2795130683@qq.com>
2018-01-27 18:51:55 +0800

    补总收入显示

--- Application/Admin/Controller/AccountsController.class.php
+++ Application/Admin/Controller/AccountsController.class.php
@@ -13,6 +13,67 @@ use Think\Controller;
  *
  **/
 class AccountsController extends PrivilegeController{
+	
+	/**
+     *  总收入
+     *
+     * @return void
+     * @author zh
+     * @since  2018.1.11
+     */
+    public function zonglists(){
+        if (I('post.start_time')){
+
+            $time1 = strtotime(I('post.start_time'));
+            if (I('post.end_time') && I('post.end_time') >= I('post.start_time')){
+                $time2 = strtotime(I('post.end_time'))-1 + 86400;
+
+            } else {
+                $time2 = strtotime(I('post.start_time'))-1 + 86400;
+            }
+            $where['creation_time'] = array( array('egt',$time1), array('elt',$time2), 'and');
+
+        }
+        
+
+        if (!$where){$where = "";}
+        list($earningsData,$show) = D('Earnings')->getzongEarningsData($where);
+		/*pr($orderData);die;*/
+
+		//当天总收入
+		$map['creation_time'] = array(
+			    array('egt',strtotime(date('Y-m-d',time()))),
+			    array('lt',strtotime(date('Y-m-d',time()))+86400)
+			);
+		/*$where['status']=2;
+		$currentOrder=M('order')->where($map)->where($where)->field('SUM(commodity_price)')->find();*/
+		/*pr($currentOrder['sum(commodity_price)']);die;*/
+		/*$currentSellOrder=M('user_sell_order')->where($map)->where($where)->field('SUM(commodity_price)')->find();
+		$sumCurrentSellOrder=$currentSellOrder['sum(commodity_price)']*0.05;
+		$sumCurrentOrder=$currentOrder['sum(commodity_price)'];
+		$sumMoney=$sumCurrentSellOrder+$sumCurrentOrder;*/
+		/*pr($sumMoney);die;*/
+		$sumM=M('earnings')->where($map)->field('SUM(price)')->find();
+		/*$sumzcM=M('earnings')->where($map)->where(' type= 1')->field('SUM(price)')->find();*/
+		/*pr($sumM['sum(price)']);die;*/
+		
+		/*pr(C('RETURN_MONEY'));die;*/
+		$biliList=M('config')->where(' id = 2')->find();
+		
+		$bili=$biliList['value'];
+		$bili=(float)$bili;
+		/*pr($bili);die;*/
+		$rSumM=$sumM['sum(price)']*$bili;
+		
+		$this->assign("bili",$bili);
+		$this->assign("sumMoney",$sumM['sum(price)']);
+		/*$this->assign("sumzcM",$sumzcM['sum(price)']);*/
+		$this->assign("rSumMoney",$rSumM);
+		
+        $this->assign("earningsData",$earningsData);
+        $this->assign("page",$show);
+        $this->display('zongaccounts');
+    }
     /**
      *  注册收入
      *
